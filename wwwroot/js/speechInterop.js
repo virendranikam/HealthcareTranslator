a95e86294aed3cac4bfa980395b5162d8ee170a4
Virendra Nikam <virendra.nikam@outlook.com>
2025-10-22 18:07:43 +0530

    Add project files.

---
+++ wwwroot/js/speechInterop.js
@@ -0,0 +1,19 @@
+﻿window.startSpeechRecognition = function (dotNetHelper) {
+    const recognition = new webkitSpeechRecognition();
+    recognition.lang = 'en-US';
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.onresult = function (event) {
+        const transcript = event.results[0][0].transcript;
+        dotNetHelper.invokeMethodAsync('ReceiveTranscript', transcript);
+    };
+
+    recognition.start();
+};
+
+window.speakText = function (text, lang) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = lang;
+    speechSynthesis.speak(utterance);
+};
